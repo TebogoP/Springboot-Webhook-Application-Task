4f9d4a4d8504bf5782e1fa51296bbd88055685f1
Tebogo <tphiri.firi@gmail.com>
2026-02-28 20:38:25 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/tebogo/webhook/SpringbootWebhookApplicationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tebogo.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebhookApplicationTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebhookApplicationTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tebogo/webhook/SpringbootWebhookApplicationTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tebogo.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebhookApplicationTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
