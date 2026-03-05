69198a1e9e900d09132f6bbbc2c5b1b19e55c60b
Tebogo <tphiri.firi@gmail.com>
2026-03-05 16:40:43 +0200

    Added extra exception handlers for a 404 and 500 in case

--- src/main/java/com/tebogo/webhook/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tebogo/webhook/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
@@ -13,9 +15,38 @@ public class GlobalExceptionHandler {
         // extract field errors
 
         // build custom response
-        APIErrorResponse response = new APIErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), (HttpStatus.BAD_REQUEST).getReasonPhrase(),ex.getMessage());
+        APIErrorResponse error = new APIErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                (HttpStatus.BAD_REQUEST).getReasonPhrase(),
+                ex.getMessage());
 
         // return ResponseEntity.badRequest().body(...)
-        return ResponseEntity.badRequest().body(response);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIErrorResponse> handle500(Exception ex) {
+
+        APIErrorResponse error = new APIErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                (HttpStatus.INTERNAL_SERVER_ERROR).getReasonPhrase(),
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<APIErrorResponse> handle404(NoHandlerFoundException ex){
+
+        APIErrorResponse error = new APIErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                (HttpStatus.NOT_FOUND).getReasonPhrase(),
+                "Endpoint does not exist \n" + ex.getMessage()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
